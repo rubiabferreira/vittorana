3c68017ea10fc05e1cd4c9980e7e0ffbf36b801c
Helio Albano de Oliveira <helioalb@gmail.com>
2015-04-08 01:43:16 -0300

    Cria scaffold 'Cliente'
    Comando usado:
    
    rails generate scaffold Cliente nome:string conjuge:string endereco:string bairro:string cep:string estado_id:integer cidade_id:integer tresidencial:string tcelular:string cpf:string rg:string evento:boolean horario_festa:time duracao_festa dia_semana:integer cortesia:boolean valor_consumacao:float itens_avulsos:integer data_evento:date qtde_criancas:integer qtde_adultos:integer tema:integer gerar_contrato:boolean valor:float num_documento:string desconto:float entrada:float forma_pagamento:string obs:text

---
+++ db/migrate/20150408044218_create_clientes.rb
@@ -0,0 +1,37 @@
+class CreateClientes < ActiveRecord::Migration
+  def change
+    create_table :clientes do |t|
+      t.string :nome
+      t.string :conjuge
+      t.string :endereco
+      t.string :bairro
+      t.string :cep
+      t.integer :estado_id
+      t.integer :cidade_id
+      t.string :tresidencial
+      t.string :tcelular
+      t.string :cpf
+      t.string :rg
+      t.boolean :evento
+      t.time :horario_festa
+      t.string :duracao_festa
+      t.integer :dia_semana
+      t.boolean :cortesia
+      t.float :valor_consumacao
+      t.integer :itens_avulsos
+      t.date :data_evento
+      t.integer :qtde_criancas
+      t.integer :qtde_adultos
+      t.integer :tema
+      t.boolean :gerar_contrato
+      t.float :valor
+      t.string :num_documento
+      t.float :desconto
+      t.float :entrada
+      t.string :forma_pagamento
+      t.text :obs
+
+      t.timestamps null: false
+    end
+  end
+end
