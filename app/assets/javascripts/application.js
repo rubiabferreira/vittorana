a1ce0b27c8cc4fb30475dc2c39adfa17c08c7150
Helio Albano de Oliveira <helioalb@gmail.com>
2015-05-18 08:49:05 -0300

    Remove tudo que está relacionado ao inputmask
    Será adcionado depois novamente.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,12 +16,6 @@
 //= require turbolinks
 //= require_tree .
 
-//= require jquery.inputmask
-//= require jquery.inputmask.extensions
-//= require jquery.inputmask.numeric.extensions
-//= require jquery.inputmask.date.extensions
-
-
 $(function(){ $(document).foundation(); });
 
 $("#campoData").mask("99/99/9999");
