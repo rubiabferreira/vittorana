fe6f48cf2154a7e1d5c27d9440f6a1a1fd51e7ad
Helio Albano de Oliveira <helioalb@gmail.com>
2015-05-24 16:34:07 -0300

    Adiciona javascript para manipular carregamento dinâmico de estados e cidades

---
+++ app/assets/javascripts/fornecedores.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  var containerEstado = $('#fornecedor_estado');
+  var containerCidade =$('#fornecedor_cidade');
+  carregaCidades(containerEstado, containerCidade);
+});
