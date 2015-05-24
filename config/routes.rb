3598a6633406e19f77925fa81bf515d4ded167de
Helio Albano de Oliveira <helioalb@gmail.com>
2015-05-24 15:04:42 -0300

    Cria rota para o metodo 'cidades' do estados_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :clientes
+
+  get 'estados/:id/cidades' => 'estados#cidades', as: :cidades_do_estado
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
