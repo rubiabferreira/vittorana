3eac6d73a5d86e148dacfc3e124d59def65b7eb4
Helio Albano de Oliveira <helioalb@gmail.com>
2015-05-25 23:28:26 -0300

    Corrige mal comportamento do javascript
    Quando o o turbolinks está ativado o javascript não carrega quando
    navegando em páginas com url's parecidas. Exemplo:
    
    host/clientes e host/clientes/5/editar
    
    Para corrigir este comportamento foi seguida esta dica:
    http://stackoverflow.com/a/18770589/2442268

--- app/assets/javascripts/clientes.js
+++ app/assets/javascripts/clientes.js
@@ -1,5 +1,8 @@
-$(document).ready(function () {
-  var containerEstado = $('#cliente_estado');
-  var containerCidade =$('#cliente_cidade');
+var ready = function () {
+  var containerEstado = $('#cliente_estado_id');
+  var containerCidade =$('#cliente_cidade_id');
   carregaCidades(containerEstado, containerCidade);
-});
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
