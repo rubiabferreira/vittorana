742a3039a02987c1441e7dac23005f4801ab8275
Helio Albano de Oliveira <helioalb@gmail.com>
2015-05-26 20:54:28 -0300

    Adiciona limite para atributos de 'evento'

--- db/migrate/20150526235005_create_eventos.rb
+++ db/migrate/20150526235005_create_eventos.rb
@@ -4,16 +4,16 @@ class CreateEventos < ActiveRecord::Migration
       t.datetime :inicio
       t.datetime :fim
       t.boolean :cortesia
-      t.decimal :consumacao
+      t.decimal :consumacao, precision: 5, scale: 2
       t.integer :itens_avulsos
       t.integer :num_criancas
       t.integer :num_adultos
       t.integer :tema_id
       t.boolean :contrato
-      t.string :documento
-      t.decimal :entrada
-      t.decimal :desconto
-      t.string :forma_pagamento
+      t.string :documento, limit: 80
+      t.decimal :entrada, precision: 5, scale: 2
+      t.decimal :desconto, precision: 5, scale: 2
+      t.string :forma_pagamento, limit: 20
       t.text :obs
       t.integer :cliente_id
 
