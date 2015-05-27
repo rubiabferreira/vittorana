4e7f6cf7febac8c3816132dafdb3ca9dd710c1cc
Helio Albano de Oliveira <helioalb@gmail.com>
2015-05-26 23:02:54 -0300

    Substitui padrão de data do rails pelo datetimepicker

--- app/assets/javascripts/eventos.js
+++ app/assets/javascripts/eventos.js
@@ -1,3 +1,10 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+var ready = function () {
+  var inicio = $('#evento_inicio');
+  var fim = $('#evento_fim');
+  inicio.datetimepicker(datetimepickerPtBr);
+  fim.datetimepicker(datetimepickerPtBr);
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
