30992f8c0ea63e04d8bca218c893f51e2bc6943a
Helio Albano de Oliveira <helioalb@gmail.com>
2015-05-31 11:55:09 -0300

    Muda regra de exclusão de clientes
    Se o cliente for excluído, todos os eventos deste cliente também serão excluídos.

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,9 +2,6 @@ class Cliente < ActiveRecord::Base
   belongs_to :estado
   belongs_to :cidade
 
-  has_many :clientes
-  # A semantica está estranha aqui.
-  # Na verdade, cliente está em muitas
-  # contas a receber da empresa.
+  has_many :eventos, dependent: :destroy
   has_many :contas_a_receber
 end
--- app/models/fornecedor.rb
+++ app/models/fornecedor.rb
@@ -2,8 +2,5 @@ class Fornecedor < ActiveRecord::Base
   belongs_to :estado
   belongs_to :cidade
   belongs_to :categoria
-  # Semântica inversa.
-  # A empresa tem muitas contas_a_pagar
-  # para o fornecedor
   has_many :contas_a_pagar
 end
