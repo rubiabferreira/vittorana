26f51d0a58f94d99d511eb75e735af0049c11733
Helio Albano de Oliveira <helioalb@gmail.com>
2015-05-31 22:13:09 -0300

    Muda a forma de exibição de clientes em contas a pagar
    Além disso foi modificada a forma de apresentação da data de vencimento.

--- app/assets/javascripts/contas_a_receber.js
+++ app/assets/javascripts/contas_a_receber.js
@@ -1,3 +1,7 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+var ready = function () {
+  var vencimento = $('#conta_a_receber_vencimento');
+  vencimento.datetimepicker(datetimepickerPtBr);
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
