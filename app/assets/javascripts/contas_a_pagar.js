1c9893357915bdcb0338fd57a89917c84dd85cb9
Rubia <rubiabferreira@gmail.com>
2015-06-03 02:45:20 -0300

    Adições no Layout

--- app/assets/javascripts/contas_a_pagar.js
+++ app/assets/javascripts/contas_a_pagar.js
@@ -1,9 +1,8 @@
 var ready = function () {
-  var vencimento = $('#conta_a_receber_vencimento');
+  var vencimento = $('#conta_a_pagar_vencimento');
   vencimento.datetimepicker(datetimepickerPtBr);
 };
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
-
 
