a0f2f3aa4a78a1f3ff6431536f527ba6216fa2eb
Helio Albano de Oliveira <helioalb@gmail.com>
2015-06-06 10:55:37 -0300

    Correção nas validações de de contas a receber

--- app/models/conta_a_receber.rb
+++ app/models/conta_a_receber.rb
@@ -1,5 +1,5 @@
 class ContaAReceber < ActiveRecord::Base
   belongs_to :cliente
 
-  validades :cliente, :vencimento, :valor, presence: :true
+  validates :cliente, :vencimento, :valor, presence: :true
 end
